08a4566b5294ef9ca234daaca4641f77c49915dc
gigi <92686026+Giselaaaa@users.noreply.github.com>
2026-09-15 17:31:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "detective-v1";
+const CACHE_NAME = "detective-v2";
 
 const ARCHIVOS = [
     "./",
@@ -9,27 +9,43 @@ const ARCHIVOS = [
 ];
 
 self.addEventListener("install", event => {
+    self.skipWaiting();
+
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(ARCHIVOS))
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ARCHIVOS);
+        })
     );
 });
 
 self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
+        caches.keys().then(keys => {
+            return Promise.all(
                 keys
                     .filter(key => key !== CACHE_NAME)
                     .map(key => caches.delete(key))
-            )
-        )
+            );
+        }).then(() => {
+            return self.clients.claim();
+        })
     );
 });
 
 self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+        fetch(event.request)
+            .then(response => {
+                const responseClone = response.clone();
+
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseClone);
+                });
+
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
     );
 });
